5f65e3b064d844b51f37d95cfe6223a571dec21e
Bobby Griffin <robertgriffintm@gmail.com>
2024-09-07 21:03:16 -0400

    move: json parsing into separate file

--- main.go
+++ main.go
@@ -1,67 +1,7 @@
 package main
 
-import (
-	"bytes"
-	"encoding/csv"
-	"encoding/json"
-	"fmt"
-	"io"
-	"log"
-	"os"
-	"strings"
-)
-
-func removeBOM(data []byte) []byte {
-	bom := []byte{0xEF, 0xBB, 0xBF}
-	if bytes.HasPrefix(data, bom) {
-		return data[len(bom):]
-	}
-	return data
-}
-
-func removeAh(s string) string {
-	return strings.Replace(s, "\u00a0", "", -1)
-}
-
 func main() {
 	filename := ReadDir()
 
-	file, err := os.Open(filename)
-	if err != nil {
-		log.Fatal(err)
-	}
-	data, err := io.ReadAll(file)
-	if err != nil {
-		fmt.Println("error reading file:", err)
-	}
-
-	cleaned := removeBOM(data)
-	var acts []Action
-	err = json.Unmarshal(cleaned, &acts)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	output, err := os.Create("act.csv")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer output.Close()
-
-	writer := csv.NewWriter(output)
-	defer writer.Flush()
-
-	header := []string{"Creation Date", "Location", "Room Number", "Recipient", "Description"}
-	if err := writer.Write(header); err != nil {
-		log.Fatal(err)
-	}
-
-	for _, r := range acts {
-		var csvRow []string
-		r.Description = removeAh(r.Description)
-		csvRow = append(csvRow, r.CreationDate, r.Location, r.RoomNumber, r.Recipient, r.Description)
-		if err := writer.Write(csvRow); err != nil {
-			log.Fatal(err)
-		}
-	}
+	ParseJson(filename, "act.csv")
 }
